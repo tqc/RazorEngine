084e09d7aa16697701511c11a4a995ea9d6f2c48
Tom Clarkson <tom@orangeguava.com>
2011-01-21 22:51:09 +1100

    add mvc version

---
+++ GenerateViewCodeWithMvc/Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web.Razor;
+using System.IO;
+using System.Web.WebPages.Razor;
+using Microsoft.CSharp;
+
+namespace GenerateViewCodeWithMvc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string templatebasename = "RazorEngine.Templating.TemplateBase";
+            if (args.Length > 0)
+            {
+                templatebasename = args[0];
+            }
+            var filecount = 0;
+            
+            // for each .cshtml file under the working directory, generate a .cs file if it has changed.
+            foreach (var templatepath in Directory.EnumerateFiles(Environment.CurrentDirectory, "*.cshtml", SearchOption.AllDirectories))
+            {
+                FileInfo fitemplate = new FileInfo(templatepath);
+                FileInfo ficode = new FileInfo(templatepath + ".cs");
+                if (!ficode.Exists || ficode.LastWriteTimeUtc < fitemplate.LastWriteTimeUtc)
+                {
+                    // get classname from path
+                    var cn = fitemplate.Name.Substring(0, fitemplate.Name.IndexOf('.'));
+                    var pt = fitemplate.DirectoryName.Split(new char[] { Path.DirectorySeparatorChar }, StringSplitOptions.RemoveEmptyEntries);
+                    var ns = pt[pt.Length - 1];
+                    for (var i = pt.Length - 2; i > 0; i--)
+                    {
+                        ns = pt[i] + "." + ns;
+                        if (pt[i + 1] == "Views") break;
+                    }
+
+                    var prp = fitemplate.FullName.Substring(fitemplate.FullName.IndexOf(@"\Views\")).Replace('\\','/');
+
+                    string template =
+                         File.ReadAllText(fitemplate.FullName);
+                    var host =
+                        new WebPageRazorHost("~" + prp, fitemplate.FullName);
+                        //new RazorEngineHost(new CSharpRazorCodeLanguage());
+                        //new WebCodeRazorHost("~"+prp, fitemplate.FullName);
+                    var rte = new RazorTemplateEngine(host);
+
+
+                    //Razor.SetTemplateBaseType(typeof(TemplateBase<>));
+
+                    string baseTypeName = templatebasename;
+                  
+                    if (template.StartsWith("@model"))
+                    {
+                        var l1 = template.IndexOf("\n");
+                        var modelTypeName = template.Substring(6, l1 - 6).Trim();
+                        template = template.Substring(l1).Trim();
+                        baseTypeName = templatebasename + "<" + modelTypeName + ">";
+                    }
+                    else if (cn == "_ViewStart")
+                    {
+                        baseTypeName = "System.Web.WebPages.StartPage";
+                    }
+                    
+                    //string result = 
+                        //Razor.ParseToCode(template, null, cn, baseTypeName, ns);
+
+                    GeneratorResults results = null;
+                    using (var reader = new StringReader(template))
+                    {
+                        results = rte.GenerateCode(reader, cn, ns, null);
+                    }
+                    StringBuilder builder = new StringBuilder();
+                                using (var writer = new StringWriter(builder))
+            {
+                new CSharpCodeProvider().GenerateCodeFromCompileUnit(results.GeneratedCode, writer, null);
+            }
+
+                    File.WriteAllText(ficode.FullName, builder.ToString());
+                    Console.WriteLine("Updated {0}.{1}", ns, cn);
+                    filecount++;
+                }
+            }
+            Console.WriteLine("Done - updated {0} files", filecount);
+        }
+
+
+    }
+}
