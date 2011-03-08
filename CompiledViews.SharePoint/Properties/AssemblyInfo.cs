ebbc967587a36480d37bb11198b9bd01d0555b02
Tom Clarkson <tom@orangeguava.com>
2011-03-08 16:08:04 +1100

    Added HtmlResult

--- CompiledViews.SharePoint/Properties/AssemblyInfo.cs
+++ CompiledViews.SharePoint/Properties/AssemblyInfo.cs
@@ -33,4 +33,4 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.36048.1054")]
+[assembly: AssemblyFileVersion("1.0.36067.1543")]
