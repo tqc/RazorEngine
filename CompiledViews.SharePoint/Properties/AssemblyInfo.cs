b8e52dc392d1c10b583b121f07fa5e645b58b1bc
Tom Clarkson <tom@orangeguava.com>
2011-05-24 10:57:43 +1000

    json content type

--- CompiledViews.SharePoint/Properties/AssemblyInfo.cs
+++ CompiledViews.SharePoint/Properties/AssemblyInfo.cs
@@ -33,4 +33,4 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.36095.1353")]
+[assembly: AssemblyFileVersion("1.0.36144.1050")]
